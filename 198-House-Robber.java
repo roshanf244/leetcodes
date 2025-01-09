eeb575e354d0bcb7aa0ce234b534db52588a25e2
roshanf244 <165632401+roshanf244@users.noreply.github.com>
2025-01-09 14:08:10 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 40.86%] - LeetPush

---
+++ 198-House-Robber.java
@@ -0,0 +1,20 @@
+class Solution {  
+    public int rob(int[] nums) {  
+        if (nums.length == 0)
+        return 0;  
+        if (nums.length == 1) 
+        return nums[0];  
+        
+        int prev1 = 0; 
+        int prev2 = 0;
+        
+        for (int num : nums) {  
+           
+            int temp = Math.max(prev1, prev2 + num);  
+            prev2 = prev1;  
+            prev1 = temp;  
+        }  
+        
+        return prev1;  
+    }  
+}
